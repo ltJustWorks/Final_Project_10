ad1291c65d4305e89eba456cddf7ad7336152a3a
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2023-11-07 11:24:37 -0500

    update course correction

--- movement_course_correction.py
+++ movement_course_correction.py
@@ -1,67 +1,51 @@
-# iteration: 2
+# iteration: 3
 from vehicle_movement import drive, wait_until_motors_done 
 from utils.brick import EV3ColorSensor, wait_ready_sensors, TouchSensor, reset_brick, busy_sleep
 import brickpi3
 from time import sleep
 import threading
+from color_classifier import classify_color
 
-COLOR_SENSOR_FERRIS = EV3ColorSensor(3)
+COLOR_SENSOR_FERRIS = EV3ColorSensor(2)
 COLOR_SENSOR_OTHER = EV3ColorSensor(4)
 BP = brickpi3.BrickPi3()
 AUX_MOTOR_1 = BP.PORT_A
 AUX_MOTOR_2 = BP.PORT_D
 POWER_LIMIT = 80
 SPEED_LIMIT = 1440
-DEFAULT_SPEED = 500
+DEFAULT_SPEED = 950
 DEFAULT_ROTATION = -840
 
 class Color:
-    ferris_norm_rgb = [0, 0, 0]
-    other_norm_rgb = [0, 0, 0]
-
     def __init__(self, ferris, other):
         self.ferris_norm_rgb = ferris
         self.other_norm_rgb = other
+    
+    def get_other_color(self):
+        return classify_color(self.other_norm_rgb)
+    
+    def get_ferris_color(self):
+        return classify_color(self.ferris_norm_rgb)
 
-    def equals(self, color_to_compare):
+    def get_color(self):
         # check if colors read are same, within some amount of variation
         # only need to check if one of the sensors reads green cause movement is not quite exact
-        other_check = True
-        ferris_check = True
-        for i in range(3):
-            if abs(color_to_compare.other_norm_rgb[i] - self.other_norm_rgb[i]) > variation:
-                other_check = False
-        for i in range(3):
-            if abs(color_to_compare.ferris_norm_rgb[i] - self.ferris_norm_rgb[i]) > variation:
-                ferris_check = False
+        # just check other_norm_rgb for now, if needed take avg of other and ferris rgb arrays
+        return self.get_other_color()
+    
 
-        return other_check or ferris_check
-
-    def ferris_equals(self, color_to_compare):
-        ferris_check = True
-        for i in range(3):
-            if abs(color_to_compare.ferris_norm_rgb[i] - self.ferris_norm_rgb[i]) > variation:
-                ferris_check = False
-        return ferris_check
-
-    def other_equals(self, color_to_compare):
-        other_check = True
-        for i in range(3):
-            if abs(color_to_compare.other_norm_rgb[i] - self.other_norm_rgb[i]) > variation:
-                other_check = False
-        return other_check
-
+# deprecated, use classify_color instead
 #green = Color([0.21, 0.52, 0.27], [0.16, 0.61, 0.22])
-green = Color([0.16, 0.61, 0.22], [0.21, 0.53, 0.27])
-brown = Color([0.55, 0.26, 0.18], [0.25, 0.36, 0.39])
-red = Color([0.83, 0.08, 0.09], [0.71, 0.16, 0.12])
-blue = Color([0.26, 0.26, 0.48], [0.25, 0.36, 0.39])
+#green = Color([0.16, 0.61, 0.22], [0.21, 0.53, 0.27])
+#brown = Color([0.55, 0.26, 0.18], [0.25, 0.36, 0.39])
+#red = Color([0.83, 0.08, 0.09], [0.71, 0.16, 0.12])
+#blue = Color([0.26, 0.26, 0.48], [0.25, 0.36, 0.39])
 
 #green_other = [0.16, 0.61, 0.22] # obtained from test readings
 #green_ferris = [0.21, 0.52, 0.27]
-variation = 0.09 # an arbitrary constant
-drive_amount = 10
-correction_amount = 20
+#variation = 0.09 # an arbitrary constant
+drive_amount = 11
+correction_amount = 30
 
 def poll_norm_rgb(sensor):
     # polls sensor data and converts to normalized rgb values
@@ -71,34 +55,47 @@ def poll_norm_rgb(sensor):
     rgb = [round(x / sum(rgb), 2) for x in rgb]
     return rgb
 
-stop_event = threading.Event()
+move_event = threading.Event()
+reverse_event = threading.Event()
 adjust_event = threading.Event()
 adjust_left_event = threading.Event()
 adjust_right_event = threading.Event()
 
 def move():
     while True:
-        if not stop_event.is_set():
-            print("should be moving")
+        if move_event.is_set():
             drive(drive_amount, drive_amount)
+        elif reverse_event.is_set():
+            drive(-drive_amount, -drive_amount)
 
         elif adjust_event.is_set():
-            print("in adjust mode")
+            #print("in adjust mode")
             if adjust_left_event.is_set():
-                print("adjusting left")
+                #print("adjusting left")
                 adjust_left()
             elif adjust_right_event.is_set():
-                print("adjusting right")
+                #print("adjusting right")
                 adjust_right()
-        
-        sleep(0.1)
 
-def turn(direction="right"):
-    while not stop_event.is_set():
+        elif turn_event.is_set():
+            if turn_left_event.is_set():
+                turn("left")
+            elif turn_right_event.is_set():
+                turn("right")
+
+        sleep(0.02)
+
+turn_event = threading.Event()
+turn_left_event = threading.Event()
+turn_right_event = threading.Event()
+
+def turn(direction):
+    if direction == "left":
+        drive(drive_amount, 0)
+    elif direction == "right":
         drive(0, drive_amount)
-        wait_until_motors_done()
 
-adjust_amount = 10
+adjust_amount = 12 # hardcoded
 def adjust_left():
     drive(adjust_amount, -adjust_amount)
 
@@ -108,99 +105,124 @@ def adjust_right():
 def sensor_poll_to_color():
     # polls data and converts to Color class
     sleep(0.1)
-    #print((poll_norm_rgb(COLOR_SENSOR_FERRIS), poll_norm_rgb(COLOR_SENSOR_OTHER)))
+    print((poll_norm_rgb(COLOR_SENSOR_FERRIS), poll_norm_rgb(COLOR_SENSOR_OTHER)), end=' ')
+    print(Color(poll_norm_rgb(COLOR_SENSOR_FERRIS), poll_norm_rgb(COLOR_SENSOR_OTHER)).get_color())
     return Color(poll_norm_rgb(COLOR_SENSOR_FERRIS), poll_norm_rgb(COLOR_SENSOR_OTHER))
-    
 
 def move_a_block():
-    move_task = threading.Thread(target=move, name='move_task')
     # color sensors are at green 
-    print("at green")
-    move_task.start()
+    #print("at green")
+    move_event.set()
     # detect when the color changes to brown
     while True:
-        polled_color = sensor_poll_to_color()
-        if not polled_color.equals(green):
-            print("break from green detected")
+        polled_color = sensor_poll_to_color().get_color()
+        if not polled_color == "green":
+            #print("break from green detected")
             break
-
 
     # detect when the color changes back to green, then stop
     while True:
         polled_color = sensor_poll_to_color()
 
         # If left sensor detects red or blue
-        if polled_color.other_equals(red) or polled_color.other_equals(blue):
-            print("Line detected, left sensor")
+        if polled_color.get_other_color() == "red" or polled_color.get_other_color() == "blue":
+            #print("Line detected, left sensor")
             # Stop moving
-            stop_event.set()
+            move_event.clear()
             # Adjust by moving left motor back and right motor forward
             adjust_event.set()
             adjust_left_event.set()
             while True:
                 polled_color = sensor_poll_to_color()
-                if not (polled_color.other_equals(red) or polled_color.other_equals(blue)):
-                    print("adjust end")
-                    sleep(0.1)
+                if not (polled_color.get_other_color() == "red" or polled_color.get_other_color() =="blue"):
+                    #print("adjust end")
+                    sleep(0.02) # hardcoded
                     adjust_left_event.clear()
                     break
             # Start moving again
             adjust_event.clear()
-            stop_event.clear()
+            move_event.set()
 
         # If right sensor detects red or blue
-        if polled_color.ferris_equals(red) or polled_color.ferris_equals(blue):
-            print("Line detected, right sensor")
+        if polled_color.get_ferris_color() == "red" or polled_color.get_ferris_color() == "blue":
+            #print("Line detected, right sensor")
             # Stop moving
-            stop_event.set()
+            move_event.clear()
             # Adjust by moving left motor back and right motor forward
             adjust_event.set()
             adjust_right_event.set()
             while True:
-                if polled_color.ferris_equals(brown):
-                    sleep(0.1)
+                polled_color = sensor_poll_to_color()
+
+                if not (polled_color.get_ferris_color() == "blue" or polled_color.get_ferris_color() == "red"):
+                    sleep(0.02)
                     adjust_left_event.clear()
                     break
             # Start moving again
             adjust_event.clear()
-            stop_event.clear()
+            move_event.set()
 
-        if polled_color.equals(brown):
-            print("brown detected")
-        if polled_color.equals(green):
+        if polled_color == "brown":
+            pass
+            #print("brown detected")
+        polled_color = sensor_poll_to_color()
+        if polled_color.get_ferris_color() == "green" or polled_color.get_ferris_color() == "green":
             print("green detected, should stop")
-            stop_event.set()
+            move_event.clear()
             break
 
-        """
-        if (polled_color.ferris_equals(blue) or polled_color.ferris_equals(red)):
-            print("line detected, ferris")
-            wait_until_motors_done()
-        elif (polled_color.other_equals(blue) or polled_color.other_equals(red)):
-            print("line detected, other")
-            wait_until_motors_done()
-        """
+    sleep(1)
 
-    move_task.join()
-    stop_event.clear()
+def turn_to_line(direction):
 
-
-def turn_to_line():
-    # drive left motor until other sensor sees red
-    turn_task = threading.Thread(target=turn, name='turn_task')
-    turn_task.start()
+    #print("turn function started")
+    turn_event.set()
+    if direction == "left":
+        turn_left_event.set()
+        while True:
+            polled_color = sensor_poll_to_color().get_ferris_color()
+            if polled_color == "red" or polled_color == "blue":
+                turn_event.clear()
+                turn_left_event.clear()
+                turn_right_event.clear()
+                # adjust a bit (hardcoded)
+                adjust_event.set()
+                adjust_right_event.set()
+                sleep(.6)
+                adjust_event.clear()
+                adjust_right_event.clear()
+                break
+    elif direction == "right":
+        turn_right_event.set()
+        while True:
+            polled_color = sensor_poll_to_color().get_other_color()
+            if polled_color == "red" or polled_color == "blue":
+                turn_event.clear()
+                turn_left_event.clear()
+                turn_right_event.clear()
+                # adjust a bit (hardcoded)
+                adjust_event.set()
+                adjust_left_event.set()
+                sleep(.6)
+                adjust_event.clear()
+                adjust_left_event.clear()
+                break
+    
+    # back up until green reached
+    reverse_event.set()
     while True:
         polled_color = sensor_poll_to_color()
-        if polled_color.other_equals(red) or polled_color.other_equals(blue):
-            stop_event.set()
+        if polled_color.get_other_color() == "green" or polled_color.get_ferris_color() == "green":
+            reverse_event.clear()
             break
-
-    turn_task.join()
-    stop_event.clear()
-
+    sleep(1)
+    #print("turning done")
+    
 # main loop
+print("before initialization")
 wait_ready_sensors(True) # Input True to see what the robot is trying to initialize! False to be silent.
-sleep(1)
+print("after initialization")
+sleep(0.5)
 
 try:
     print("BrickPi Motos Position Demo:")
@@ -216,8 +238,48 @@ try:
         print(error)
     
     try:
-        move_a_block()
-        turn_to_line()
+        move_task = threading.Thread(target=move, name='turn_task')
+        move_task.start()
+
+        for i in range(4):
+            print(f"turn number {i}")
+            for j in range(3):
+                print(f"move number: {j}")
+                move_a_block()
+                while True:
+                    if not move_event.is_set():
+                        break
+            
+            turn_to_line("left")
+            while True:
+                if not move_event.is_set():
+                    break
+
+        """
+        for i in range(2):
+            move_a_block()
+            sleep(0.1)
+            while True:
+                if not move_event.is_set():
+                    break
+            turn_to_line("left")
+            while True:
+                print("turning")
+                if not move_event.is_set():
+                    print("turning done")
+                    break
+            move_a_block()
+            while True:
+                if not move_event.is_set():
+                    break
+            turn_to_line("right")
+            while True:
+                if not move_event.is_set():
+                    break
+        """
+
+        move_task.join()
+
     except IOError as error:
         print(error)
 except KeyboardInterrupt:
