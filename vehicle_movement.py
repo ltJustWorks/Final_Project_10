7c15ad0dd95e2ac30009a38ee3cf3fbd74d55a98
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2023-11-07 11:24:49 -0500

    change hardcoded movement constants

--- vehicle_movement.py
+++ vehicle_movement.py
@@ -9,7 +9,7 @@ AUX_MOTOR_1 = BP.PORT_A
 AUX_MOTOR_2 = BP.PORT_D
 POWER_LIMIT = 80
 SPEED_LIMIT = 1440
-DEFAULT_SPEED = 250
+DEFAULT_SPEED = 950
 DEFAULT_ROTATION = -840
 
 def is_moving():
@@ -17,13 +17,13 @@ def is_moving():
 
 def wait_until_motors_done():
     while not is_moving():
-        busy_sleep(0.02)
-    while is_moving():
         busy_sleep(0.05)
+    while is_moving():
+        busy_sleep(0.1)
 
 def drive(left_rotation, right_rotation):
     BP.set_motor_limits (AUX_MOTOR_1, POWER_LIMIT, DEFAULT_SPEED)
-    BP.set_motor_position_relative (AUX_MOTOR_1, left_rotation)
+    BP.set_motor_position_relative (AUX_MOTOR_1, left_rotation*1.25) #hardcoded to deal with right drift
     BP.set_motor_limits (AUX_MOTOR_2, POWER_LIMIT, DEFAULT_SPEED)
     BP.set_motor_position_relative (AUX_MOTOR_2, right_rotation)
 
